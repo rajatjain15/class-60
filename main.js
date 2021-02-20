249875dbdbcf2f1295b0d2d0148bc96c961ecc1e
rajatjain15 <79210039+rajatjain15@users.noreply.github.com>
2021-02-20 12:41:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function changecolor(){
+document.getElementById("hobby").style.color="red"
+
+}
+
+
+
+
+
+
+
+
+
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvVa81QuHkZpCu-oBgXTo7b6I8T2CkJCnjKQ&usqp=CAU" dhoni img
+
+
+
+
+
+
+
+  <iframe width="853" height="480" src="https://www.youtube.com/embed/b4OH3vBANa4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <div class="carousel-content"> dhoni viedio
